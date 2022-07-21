1b7841d204d611e13e3428c7c78522fbed262aa2
afidhehe <miftahullafid@gmail.com>
2022-07-21 21:52:24 +0700

    add notes file

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+"""
+semua sintaksis dasar bahasa pemrograman terdiri dari :
+1. Sekuensial
+2. Percabangan
+3. Perulangan
+"""
+
+#lets we try 1st trial
+
+print("Saya mengajukan penawaran kerjasama dengan pak Maman")
+print('kemudian maman menjawab akan segera diproses')
+print('kemudian maman menjawab akan segera diproses')
--- main.py
+++ main.py
@@ -2,3 +2,5 @@
 This is a demo version w/ Mr. Eko
 """
 print ('hello dunia!!!')
+print ('semakin cepat menggunakan vim yang dipadu dengan pyCharm')
+print('hari ke 2 berlatih dengan PyCharm dengan plugin ideaVim')
